b0f7df4b4ef0800427acfd5f7af0a59715b72c65
Matthew Rocklin <mrocklin@gmail.com>
2012-10-30 11:30:40 -0500

    sympy unify works

--- test_sympy.py
+++ test_sympy.py
@@ -1,8 +1,26 @@
-from sympy import Add, Basic
+from sympy import Add, Basic, Wild
 from unify import *
-from unify_sympy import destruct, construct
+from unify_sympy import destruct, construct, unify
 
 def test_destruct():
     expr     = Basic(1, 2, 3)
     expected = Compound(Basic, (1, 2, 3))
     assert destruct(expr) == expected
+
+def test_construct():
+    expr     = Compound(Basic, (1, 2, 3))
+    expected = Basic(1, 2, 3)
+    assert construct(expr) == expected
+
+def test_nested():
+    expr = Basic(1, Basic(2), 3)
+    cmpd = Compound(Basic, (1, Compound(Basic, (2,)), 3))
+    assert destruct(expr) == cmpd
+    print construct(cmpd)
+    assert construct(cmpd) == expr
+
+def test_unify():
+    expr = Basic(1, 2, 3)
+    a, b, c = map(Wild, 'abc')
+    pattern = Basic(a, b, c)
+    assert list(unify(expr, pattern, {})) == [{a: 1, b: 2, c: 3}]
--- unify_sympy.py
+++ unify_sympy.py
@@ -1,14 +1,23 @@
-from sympy import Basic
-from unify import Compound
+from sympy import Basic, Wild
+from unify import Compound, Variable, _unify
 
 def destruct(s):
     """ Turn a SymPy object into a Compound Tuple """
     if not isinstance(s, Basic):
         return s
+    if isinstance(s, Wild):
+        return Variable(s)
     return Compound(s.__class__, tuple(map(destruct, s.args)))
 
 def construct(t):
     """ Turn a Compount Tuple into a SymPy object """
+    if isinstance(t, Variable):
+        return t.arg
     if not isinstance(t, Compound):
         return t
-    return t.op(*t.args)
+    return t.op(*map(construct, t.args))
+
+def unify(x, y, s):
+    ds = _unify(destruct(x), destruct(y), {})
+    for d in ds:
+        yield {construct(k): construct(v) for k, v in d.items()}
