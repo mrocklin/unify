439a0957538e0b3e29431ddadda929a4f5abaa63
Matthew Rocklin <mrocklin@gmail.com>
2012-10-30 12:21:29 -0500

    add tentative docstring for sympy unify

--- unify_sympy.py
+++ unify_sympy.py
@@ -18,6 +18,22 @@ def construct(t):
     return Basic.__new__(t.op, *map(construct, t.args))
 
 def unify(x, y, s):
+    """ Structural unification of two expressions possibly containing Wilds
+
+    >>> from unify_sympy import unify
+    >>> from sympy import Wild
+    >>> from sympy.abc import x, y, z
+    >>> expr = 2*x + y + z
+    >>> pattern = 2*Wild('p') + Wild('q')
+    >>> list(unify(expr, pattern, {}))
+    [{p_: x, q_: y + z}]
+
+    >>> expr = x + y + z
+    >>> pattern = Wild('p') + Wild('q')
+    >>> list(unify(expr, pattern, {}))
+    [{p_: z, q_: x + y}, {p_: y + z, q_: x}]
+    """
+
     ds = _unify(destruct(x), destruct(y), {})
     for d in ds:
         yield {construct(k): construct(v) for k, v in d.items()}
